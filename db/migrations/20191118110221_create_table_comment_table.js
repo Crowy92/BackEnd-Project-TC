1d813692a48163b7d5f25a1da159289bcabd04a7
Thomas Crowther <tom.crowther1992@gmail.com>
2019-12-23 10:56:41 +0000

    cascading delete enabled

--- db/migrations/20191118110221_create_table_comment_table.js
+++ db/migrations/20191118110221_create_table_comment_table.js
@@ -3,7 +3,9 @@ exports.up = function (knex) {
     return knex.schema.createTable('comments', (commentTable) => {
         commentTable.increments('comment_id').primary();
         commentTable.string('author').references('users.username');
-        commentTable.integer('article_id').references('articles.article_id');
+        commentTable.integer('article_id').references('articles.article_id')
+            .onUpdate('CASCADE') // if Article primary key is changed, update this foreign key.
+            .onDelete('CASCADE') // if referenced Article is deleted, delete this Comment.;
         commentTable.integer('votes');
         commentTable.timestamp('created_at', { precision: 6 }).defaultTo(knex.fn.now());
         commentTable.text('body').notNullable();
